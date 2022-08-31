a1d931e88dcd03465901751de6913f245156c612
Igli-Boci <igliboci10@gmai.com>
2022-08-31 03:45:01 +0200

    Adding cors to the server

--- index.ts
+++ index.ts
@@ -1,14 +1,14 @@
 import express from "express";
-
+import cors from "cors";
 
 const app = express();
 const port = 3010;
+app.use(cors());
 
 app.get("/", (req, res) => {
-    res.send("<h1>Home Page</h1>");
-  });
-
+  res.send("<h1>Home Page</h1>");
+});
 
 app.listen(port, function () {
-    console.log(`Server is Online on ${port}`);
-  });
+  console.log(`Server is Online on ${port}`);
+});
